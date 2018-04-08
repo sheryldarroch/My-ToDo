92dab2c46b0f39e5a89dc81c4e7730b3b4c7467f
sheryldarroch <sheryld1028@gmail.com>
2018-04-08 09:07:22 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const app = express();
+
+// mongodb connection using mongoose
+mongoose.connect("mongodb://localhost:27017/ToDo")
+const db = mongoose.connection;
+
+// use sessions for tracking logins
+app.use(session({
+  secret: 'to do lists are awesome',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// make user ID available to templates
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.userId;
+  next();
+});
+
+//mongo console.error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//parse incoming request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// serve static files from /public
+app.use(express.static(__dirname + '/public'));
+
+//view engine setup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+// include routes
+const routes = require('./routes/index');
+app.use(routes);
+
+//catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+//error handler
+//define as the last app.use callback
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+// listen on port 3000
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000!')
+});
---
+++ routes/index.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+//GET /profile
+router.get('/profile', (req, res, next) => {
+  if(! req.session.userId) {
+    let err = new Error('You are not logged in!');
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+    .exec((error, user) => {
+      if(error) {
+        return next(error);
+      } else {
+        return res.render('profile', {title: 'Profile', name: user.name});
+      }
+    });
+});
+
+// GET /Logout
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    //delet session object
+    req.session.destroy((err) =>{
+      if(err) {
+        return next(err);
+      } else {
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// GET /login
+router.get('/login', (req, res, next) => {
+  return res.render('login', {title: 'Log In'});
+});
+
+// POST /login
+router.post('/login', (req, res, next) => {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if(error || !user) {
+        let err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    let err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
+});
+
+// GET /register
+router.get('/register', (req, res, next) => {
+  return res.render('register', {title: 'Sign Up'});
+});
+
+//POST /register
+router.post('/register', (req, res, next) => {
+  if (req.body.email &&
+    req.body.name &&
+    req.body.password &&
+    req.body.confirmPassword) {
+
+      //confirm that user typed same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        const err = new Error('Passwords do NOT match!');
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      let userData = {
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password
+      };
+
+      // use schema's create method to insert document into Mongo
+      User.create(userData, function(error, user) {
+        if(error) {
+          return next(error);
+        } else {
+          req.session.userId = user._id;
+          return res.redirect('/profile');
+        }
+      });
+
+    } else {
+      const err = new Error('All fields required');
+      err.status = 400;
+      return next(err);
+    }
+});
+
+// GET /
+router.get('/', (req, res, next) => {
+  return res.render('index', {title: 'Home'});
+});
+
+// GET /about
+router.get('/about', (req, res, next) => {
+  return res.render('about', {title: "About"});
+});
+
+
+module.exports = router;
